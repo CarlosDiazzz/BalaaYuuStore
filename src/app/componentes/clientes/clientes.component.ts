44471e21540e11ec27ca32b842b373cef916ca1e
CarlosDiaz <carlosdiazvasquez0406@gmail.com>
2025-04-18 00:03:11 -0600

    Se guardo l oque es el cliente

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../../modelo/cliente.modelo';
+import { Observable } from 'rxjs';
+import { ClienteService } from '../../servicios/cliente.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Added RouterModule import
 
 @Component({
-  selector: 'app-clientes',
-  imports: [],
+  // Removed invalid 'imports' property
+  imports: [CommonModule, RouterModule],
   templateUrl: './clientes.component.html',
-  styleUrl: './clientes.component.css'
+  styleUrl: './clientes.component.css',
 })
 export class ClientesComponent {
+  clientes: Cliente[] | null = null;
 
+  constructor(private clienteService: ClienteService) {}
+  ngOnInit() {
+    this.clienteService.getClientes().subscribe((clientes) => {
+      this.clientes = clientes;
+    });
+  }
 }
--- src/app/componentes/tablero/tablero.component.ts
+++ src/app/componentes/tablero/tablero.component.ts
@@ -3,7 +3,6 @@ import { ClientesComponent } from "../clientes/clientes.component";
 
 @Component({
   selector: 'app-tablero',
-  imports: [ClientesComponent],
   templateUrl: './tablero.component.html',
   styleUrl: './tablero.component.css'
 })
