af359d801390d55e276ab348a5ee3d480c0153a4
AlejandroVill <alejandrozzzxx@gmail.com>
2025-04-29 14:26:04 -0600

    Modifique los colores y logica del pie de pagina, cabecero, styles

--- src/app/componentes/cabecero/cabecero.component.ts
+++ src/app/componentes/cabecero/cabecero.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { LoginService } from '../../servicios/login.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cabecero',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './cabecero.component.html',
   styleUrl: './cabecero.component.css'
 })
-export class CabeceroComponent {
+export class CabeceroComponent implements OnInit {
 
   isLoggedIn: boolean = false;
   loggedInUser: string | null = null;//alamcena el email del usuario logueado
--- src/app/componentes/pie-pagina/pie-pagina.component.ts
+++ src/app/componentes/pie-pagina/pie-pagina.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pie-pagina',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './pie-pagina.component.html',
   styleUrl: './pie-pagina.component.css'
 })
 export class PiePaginaComponent {
+  // Año actual para el copyright
+  currentYear = new Date().getFullYear();
 
+  // Datos de contacto
+  contacto = {
+    telefono: '+52 951 123 4567',
+    email: 'info@balaayuu.com',
+    direccion: 'Oaxaca de Juárez, Oaxaca, México'
+  };
+
+  // Enlaces a redes sociales
+  redesSociales = [
+    { nombre: 'Facebook', icono: 'bi-facebook', url: 'https://facebook.com/balaayuu' },
+    { nombre: 'Instagram', icono: 'bi-instagram', url: 'https://instagram.com/balaayuu' },
+    { nombre: 'Twitter', icono: 'bi-twitter', url: 'https://twitter.com/balaayuu' },
+    { nombre: 'YouTube', icono: 'bi-youtube', url: 'https://youtube.com/balaayuu' }
+  ];
 }
