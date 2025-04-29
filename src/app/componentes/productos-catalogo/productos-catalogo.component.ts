06f5b0ac2a6ae07de908ea12d52a7ce74b9cc252
Carlos Diaz <carlosdiazvasquez0406@gmail.com>
2025-04-29 10:42:06 -0600

    productos y ya

---
+++ src/app/componentes/productos-catalogo/productos-catalogo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-catalogo',
+  imports: [],
+  templateUrl: './productos-catalogo.component.html',
+  styleUrl: './productos-catalogo.component.css'
+})
+export class ProductosCatalogoComponent {
+
+}
